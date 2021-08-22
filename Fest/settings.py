bb49f08ac4a13a8aaf2719751eeb1000f68a390f
Amir Pathan <amir.pathan.180@gmail.com>
2021-08-22 19:04:10 +0530

    defult = none

--- Fest/settings.py
+++ Fest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nd^f$)!vx0c(vsh3y)ikd6f**42r_4z4^+jm_$pdy-n0)idk*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fest1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
