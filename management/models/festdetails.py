c022f61700b69754eec009ebda9f909c515faf66
Amir Pathan <amir.pathan.180@gmail.com>
2021-09-23 09:21:31 +0530

    first commit

---
+++ management/models/festdetails.py
@@ -0,0 +1,18 @@
+from django.db import models
+# Create your models here.
+
+
+class Participant(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    phone = models.CharField(max_length=11, default=None)
+    email = models.EmailField(default=None)
+    password = models.CharField(max_length=254, default=None)
+
+
+class SignInUser(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    phone = models.CharField(max_length=11, default=None)
+    email = models.EmailField(default=None)
+    password = models.CharField(max_length=254, default=None)
